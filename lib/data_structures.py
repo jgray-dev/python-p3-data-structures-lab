43b01a3125bd3f9b08c1ea46438d70672d05161a
Jackson Gray <nohaxjustdoge@gmail.com>
2024-04-02 20:40:05 +0000

    i hate python!

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,46 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    names_list = list()
+    for food in spicy_foods:
+        names_list.append(food["name"])
+    return names_list
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy_list = list()
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spicy_list.append(food)
+    return spicy_list
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: " + "🌶" * food["heat_level"])
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: " + "🌶" * food["heat_level"])
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    x = int()
+    for food in spicy_foods:
+        x += food["heat_level"]
+    return x / len(spicy_foods)
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
